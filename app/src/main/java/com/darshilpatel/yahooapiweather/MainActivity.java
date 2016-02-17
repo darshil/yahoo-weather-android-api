5abf21f6571b4780433a8dc0bb942731ce7dc2bc
Owner <darshil-patel@hotmail.com>
2016-02-16 19:05:47 -0500

    Fixed JSONFetcher

--- app/src/main/java/com/darshilpatel/yahooapiweather/MainActivity.java
+++ app/src/main/java/com/darshilpatel/yahooapiweather/MainActivity.java
@@ -8,15 +8,47 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.darshilpatel.yahooapiweather.data.Channel;
+import com.darshilpatel.yahooapiweather.service.WeatherServiceCallback;
+import com.darshilpatel.yahooapiweather.service.YahooWeatherService;
+
+public class MainActivity extends AppCompatActivity implements WeatherServiceCallback {
+
+    private ImageView weatherIconImageView;
+    private TextView temperatureTextView;
+    private TextView conditionTextView;
+    private TextView locationTextView;
+
+
+    private YahooWeatherService service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        weatherIconImageView = (ImageView)findViewById(R.id.weatherIconImageView);
+        temperatureTextView = (TextView)findViewById(R.id.tempratureTextView);
+        conditionTextView = (TextView)findViewById(R.id.conditionTextView);
+        locationTextView = (TextView)findViewById(R.id.locationTextView);
+
+        service = new YahooWeatherService(this);
+        service.refreshWeather("Brampton, ON");
+
     }
 
 
+    @Override
+    public void serviceSuccess(Channel channel) {
+
+    }
+
+    @Override
+    public void serviceFailure(Exception exception) {
+        Toast.makeText(this, exception.getMessage(), Toast.LENGTH_LONG).show();
+    }
 }
--- app/src/main/java/com/darshilpatel/yahooapiweather/service/YahooWeatherService.java
+++ app/src/main/java/com/darshilpatel/yahooapiweather/service/YahooWeatherService.java
@@ -1,5 +1,7 @@
 package com.darshilpatel.yahooapiweather.service;
 
+import android.os.AsyncTask;
+
 /**
  * Created by Darshil on 2/16/2016.
  */
@@ -7,6 +9,25 @@ public class YahooWeatherService{
     private WeatherServiceCallback callback;
     private String location;
 
-    
+    public YahooWeatherService(WeatherServiceCallback callback) {
+        this.callback = callback;
+    }
 
+    public String getLocation(){
+        return location;
+    }
+    public void refreshWeather(String location){
+
+        new AsyncTask<String, Void, String>() {
+            @Override
+            protected String doInBackground(String... params) {
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String s){
+                super.onPostExecute(s);
+            }
+        }.execute(location);
+    }
 }
