8f3aa78a5e85919a84c95962f3731f89a16c5bfb
darshilpatel <darshil-patel@hotmail.com>
2016-03-16 12:57:53 -0400

    restart

---
+++ AlphaWeather/app/src/androidTest/java/com/darshilpatel/alphaweather/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.darshilpatel.alphaweather;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
